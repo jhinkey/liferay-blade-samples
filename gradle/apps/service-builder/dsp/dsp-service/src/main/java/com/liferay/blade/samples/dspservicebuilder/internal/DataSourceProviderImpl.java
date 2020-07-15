6208d2506ffaad0e251a14ff66110ce4197aa204
James Hinkey <james.hinkey@liferay.com>
2020-07-15 13:24:32 -0400

    set jndi.name in a property

--- gradle/apps/service-builder/dsp/dsp-service/src/main/java/com/liferay/blade/samples/dspservicebuilder/internal/DataSourceProviderImpl.java
+++ gradle/apps/service-builder/dsp/dsp-service/src/main/java/com/liferay/blade/samples/dspservicebuilder/internal/DataSourceProviderImpl.java
@@ -4,6 +4,7 @@ import com.liferay.portal.kernel.dao.jdbc.DataSourceFactoryUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataSourceProvider;
 import com.liferay.portal.kernel.util.PropsUtil;
 
+import java.util.Properties;
 import javax.sql.DataSource;
 
 public class DataSourceProviderImpl implements DataSourceProvider {
@@ -11,8 +12,13 @@ public class DataSourceProviderImpl implements DataSourceProvider {
 	@Override
 	public DataSource getDataSource() {
 		try {
-			return DataSourceFactoryUtil.initDataSource(
-                null, null, null, null, "jdbc/externalDataSource");
+
+            Properties properties = new Properties();
+
+            String jndiName = "jdbc/externalDataSource";
+            properties.setProperty("jndi.name", jndiName);
+
+            return DataSourceFactoryUtil.initDataSource(properties);
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
