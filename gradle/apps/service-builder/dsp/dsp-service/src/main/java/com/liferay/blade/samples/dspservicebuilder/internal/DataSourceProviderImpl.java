3aa4c3f15686eb93cd5be456b10e013aabfc8dfb
James Hinkey <james.hinkey@liferay.com>
2020-07-15 15:05:10 -0400

    do jndi lookup

--- gradle/apps/service-builder/dsp/dsp-service/src/main/java/com/liferay/blade/samples/dspservicebuilder/internal/DataSourceProviderImpl.java
+++ gradle/apps/service-builder/dsp/dsp-service/src/main/java/com/liferay/blade/samples/dspservicebuilder/internal/DataSourceProviderImpl.java
@@ -2,26 +2,36 @@ package com.liferay.blade.samples.dspservicebuilder.internal;
 
 import com.liferay.portal.kernel.dao.jdbc.DataSourceFactoryUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataSourceProvider;
+import com.liferay.portal.kernel.jndi.JNDIUtil;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 
 import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
 
 public class DataSourceProviderImpl implements DataSourceProvider {
 
-	@Override
+    @Override
 	public DataSource getDataSource() {
+		Thread currentThread = Thread.currentThread();
+		ClassLoader classLoader = currentThread.getContextClassLoader();
+		currentThread.setContextClassLoader(PortalClassLoaderUtil.getClassLoader());
 		try {
-
-            Properties properties = new Properties();
-
-            String jndiName = "jdbc/externalDataSource";
-            properties.setProperty("jndi.name", jndiName);
-
-            return DataSourceFactoryUtil.initDataSource(properties);
+			Properties jndiEnvironmentProperties = PropsUtil.getProperties(
+				PropsKeys.JNDI_ENVIRONMENT, true);
+			Context context = new InitialContext(jndiEnvironmentProperties);
+			return (DataSource)JNDIUtil.lookup(
+				context, "jdbc/externalDataSource");
 		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+		finally {
+			currentThread.setContextClassLoader(classLoader);
 		}
 	}
 
